2d7674634fea2775218e82100eb81a40a3a1cd9f
ryoma <c0a23040d7@edu.teu.ac.jp>
2025-06-10 15:51:43 +0900

    練習(10)

--- pygame_image.py
+++ pygame_image.py
@@ -13,17 +13,29 @@ def main():
     rbg_img = pg.transform.flip(bg_img, True, False)
     kt_img = pg.image.load("fig/3.png")
     rkt_img = pg.transform.flip(kt_img, True, False)
+    kt_rct = kt_img.get_rect()
+    kt_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kt_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kt_rct.move_ip((0, 1))
+        if key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((1, 0))
+        if key_lst[pg.K_LEFT]:
+            kt_rct.move_ip((-1, 0))
 
         if tmr == 3200:
             tmr = 0
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(rbg_img, [1600-tmr, 0])
         screen.blit(bg_img, [3200-tmr, 0])
-        screen.blit(rkt_img, [300, 200])
+        screen.blit(rkt_img, kt_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
